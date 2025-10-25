7aae827d3988c72f3424d2371a414be2f2056757
medvedicos <41338299+medvedicos@users.noreply.github.com>
2025-10-25 12:38:03 +0300

    Update install_up.sh

--- install_up.sh
+++ install_up.sh
@@ -3,5 +3,3 @@
 #Скачивание обновления
 curl -f https://raw.githubusercontent.com/medvedicos/Update-Firmware/refs/heads/main/update --output /etc/init.d/update
 chmod +x /etc/init.d/update
-/etc/init.d/update enable
-/etc/init.d/update start
